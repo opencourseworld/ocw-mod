913366d1e53bef97edebbac97e01c59d17d2aead
jneen <jneen@jneen.net>
2025-06-28 14:18:26 -0400

    allow running other actions inside exlaunch.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 set -xe
-docker run -it --rm -w /work -v $PWD:/work devkitpro/devkita64 ./exlaunch.sh build
+ACTION="$1"
+
+if [[ -z "$ACTION" ]]; then ACTION=build; fi
+
+docker run -it --rm -w /work -v $PWD:/work devkitpro/devkita64 ./exlaunch.sh "$ACTION"
