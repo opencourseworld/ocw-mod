0d63ce0ea000b447ccf022f9f9577351c3c84cf2
jneen <jneen@jneen.net>
2025-07-04 00:13:29 -0400

    put the version detection back

--- source/program/version.hpp
+++ source/program/version.hpp
@@ -15,20 +15,19 @@ namespace exl::util {
 
     namespace impl {
         ALWAYS_INLINE UserVersion DetermineUserVersion() {
-            return UserVersion::V3_0_2;
-            // /* 
-            //     Implement a method of determining your application version here if you want to take advantage of this feature.
-            //     An example may be apply Murmur3 over the .text and .rodata of the main executable or calling nn::oe::GetDisplayVersion.
-            //     The former would have complications if other mods are in effect, and the latter may have issues with people doing safe downgrades on console.
-            // */
-            // nn::oe::DisplayVersion displayVersion;
-            // nn::oe::GetDisplayVersion(&displayVersion);
+            /*
+                Implement a method of determining your application version here if you want to take advantage of this feature.
+                An example may be apply Murmur3 over the .text and .rodata of the main executable or calling nn::oe::GetDisplayVersion.
+                The former would have complications if other mods are in effect, and the latter may have issues with people doing safe downgrades on console.
+            */
+            nn::oe::DisplayVersion displayVersion;
+            nn::oe::GetDisplayVersion(&displayVersion);
 
-            // if (strncmp(displayVersion.name, "3.0.2", 6) == 0) {
-            //     return UserVersion::V3_0_2;
-            // } else {
-            //     return UserVersion::DEFAULT;
-            // }
+            if (strncmp(displayVersion.name, "3.0.2", 6) == 0) {
+                return UserVersion::V3_0_2;
+            } else {
+                return UserVersion::DEFAULT;
+            }
         }
     }
 }
